ca16a8a1fece28f593534fab66e141c94fa8bbbe
Francisco Javier Garrido Peso <fran.garrido.peso@gmail.com>
2024-09-30 20:55:43 +0200

    Added menu to calculator and linked add operation

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,37 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Welcome to our calculator!");
+
+        menu();
+
+    }
+    public static void menu(){
         Operators operators = new Operators();
 
-        System.out.println(operators.add(7,3));
+        System.out.println("1. Add");
+        System.out.println("2. Subtract");
+        System.out.println("3. Multiply");
+        System.out.println("4. Divide");
+        System.out.println("5. Exit");
+
+        System.out.println("Enter your choice: ");
+        Scanner scanner = new Scanner(System.in);
+        int choice = scanner.nextInt();
+
+        switch(choice) {
+            case 1:
+                System.out.println("Enter first number: ");
+                int num1 = scanner.nextInt();
+                System.out.println("Enter second number: ");
+                int num2 = scanner.nextInt();
+                System.out.println(operators.add(num1,num2));
+                menu();
+                break;
+            case 0:
+                System.out.println("Goodbye!");
+                break;
+        }
     }
 }
