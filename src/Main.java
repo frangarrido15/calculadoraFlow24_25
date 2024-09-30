3ad73c0755309712f0fe3f17db0c21ca3fb5ab49
Francisco Javier Garrido Peso <fran.garrido.peso@gmail.com>
2024-09-30 20:39:06 +0200

    Added sum method and testes

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Welcome to our calculator!");
+        Operators operators = new Operators();
+
+        System.out.println(operators.add(7,3));
     }
 }
--- src/Operators.java
+++ src/Operators.java
@@ -1,2 +1,5 @@
 public class Operators {
+    public int add(int a, int b) {
+        return a + b;
+    }
 }
